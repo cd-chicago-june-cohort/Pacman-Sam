b0c6879efe0b5d68115c316cae144a199bd6155c
sozo2 <sam.zoeller@gmail.com>
2017-07-02 00:31:58 -0500

    please save

--- ghosts.js
+++ ghosts.js
@@ -1,8 +1,6 @@
-//setInterval(ghosts(), 300);
 
-//ex: var ghostPos = {x: 6, y: 1};
 
-function ghostStrategy(ghostPos, playerPos) {
+function blueStrategy(ghostPos, playerPos) {
     var neighbors = findNeighbors(grid, ghostPos);
     var moves = [neighbors.down, neighbors.left, neighbors.up, neighbors.right];
     if (playerPos.y > ghostPos.y) {
@@ -23,40 +21,41 @@ function ghostStrategy(ghostPos, playerPos) {
     }
     for (var i = 0; i < moves.length; i++) {
         var destVal = getVal(grid, moves[i]);
-        if (destVal !== null && destVal !== WALL && destVal !== TUNL) {
+        if (destVal !== null && destVal !== WALL && destVal !== TUNL && destVal != ORNG) {
             var ghostDest = moves[i];
             return ghostDest;
         }
     }
 }
 
-///interval function should only pass out new location
+function orangeStrategy(ghostPos, playerPos) {
+    var neighbors = findNeighbors(grid, ghostPos);
+    var moves = [neighbors.down, neighbors.left, neighbors.up, neighbors.right];
+    if (playerPos.x > ghostPos.x) {
+        moves[0] = neighbors.right;
+        moves[2] = neighbors.left;
+    };
+    if (playerPos.x < ghostPos.x) {
+        moves[0] = neighbors.left;
+        moves[2] = neighbors.right;
+    }
+    if (playerPos.y > ghostPos.y) {
+        moves[1] = neighbors.down;
+        moves[3]= neighbors.up;
+    };
+    if (playerPos.y < ghostPos.y) {
+        moves[1] = neighbors.up;
+        moves[3] = neighbors.down;
+    }
+    for (var i = 0; i < moves.length; i++) {
+        var destVal = getVal(grid, moves[i]);
+        if (destVal !== null && destVal !== WALL && destVal !== TUNL && destVal != ORNG) {
+            var ghostDest = moves[i];
+            return ghostDest;
+        }
+    }
+}
 
-
-
-// //var ghostPos;
-// var ghostDest = ghostStrategy(ghostPos, playerPos);
-
-
-// function ghostMove() {
-//     var swapVal = getVal(grid, ghostDest);
-//     setValue(grid, ghostDest, ORNG);
-//     setValue(grid, ghostPos, swapVal);
-//     ghostPos = ghostDest;
-//     updateDisplay();
-// }
-
-// function ghosts(ghost, ghostPos, playerPos) {
-//     var ghostDest = ghostStrategy(ghostPos, playerPos);
-//     if (ghostDest !== null && ghostDest !== WALL) {
-//         return false;
-//     }
-//     var currentValue = getVal(grid, ghostPos);
-//     setValue(grid, ghostDest, ghost);
-//     setValue(grid, ghostPos, currentValue);
-//     ghostPos = ghostDest;
-//     return true;
-// }
 
 
 
